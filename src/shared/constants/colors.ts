61066b3437bea3cf37f724e2092cd09af6d6fe4f
toniebe <toniebe48@gmail.com>
2023-03-12 03:37:38 +0700

    fix[style]: adding some color in shared colors

--- src/shared/constants/colors.ts
+++ src/shared/constants/colors.ts
@@ -1,24 +1,35 @@
-const bluePrimary = "#039ffb";
-const blueSecondary = "#6bc3fc";
-const blueAlternative3 = "#e7f5fe";
-const textDark = '#697782';
-const textDark1 = "#111733";
-const textDark2 = "#738188";
-const textDark3 = "#9ea2a5";
+const bluePrimary = '#0aa0fb';
+const blueSecondary = '#6bc3fc';
+const blueAlternative1 = '#2eaffd';
+const blueAlternative3 = '#e7f5fe';
+const textGrey = '#b0b6bb';
+const textDark = '#000';
+const textDark1 = '#111733';
+const textDark2 = '#738188';
+const textDark3 = '#9ea2a5';
 const whiteColor = '#ffffff';
 const blackColor = '#111733';
-
-
-
+const redWarning = '#fc8f8f';
+const redText = '#fd6767';
+const yellowPrimary = '#fcca4d';
+const greenPrimary = '#2ccb64';
+const purplePrimary = '#4e41b3';
 
 export {
-    bluePrimary,
-    blueSecondary,
-    blueAlternative3,
-    textDark,
-    textDark1,
-    textDark2,
-    textDark3,
-    whiteColor,
-    blackColor
-}
+  bluePrimary,
+  blueSecondary,
+  blueAlternative1,
+  blueAlternative3,
+  textGrey,
+  textDark,
+  textDark1,
+  textDark2,
+  textDark3,
+  whiteColor,
+  blackColor,
+  redWarning,
+  redText,
+  yellowPrimary,
+  greenPrimary,
+  purplePrimary,
+};
