c782f769fbc2f33be5e14d33728d384c4abe6611
toniebe <toniebe48@gmail.com>
2023-03-11 22:42:36 +0700

    feat: create core module

---
+++ src/core/index.tsx
@@ -0,0 +1,32 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import SplashContainer from './SplashScreen/container/SplashContainer';
+import BottomRouter from './router/BottomRouter';
+
+const Core = () => {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SplashScreen">
+        <Stack.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="SplashScreen"
+          component={SplashContainer}
+        />
+        <Stack.Screen
+          name="Dashboard"
+          component={BottomRouter}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Core;
