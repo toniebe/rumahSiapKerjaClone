704047bff1792af41633ab8e3e249cc8e377058f
toniebe <toniebe48@gmail.com>
2023-03-11 22:35:23 +0700

    feat[component] create component button

---
+++ src/shared/components/atom/Button/Button.tsx
@@ -0,0 +1,109 @@
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+} from 'react-native';
+import React from 'react';
+import {
+  bluePrimary,
+  textDark1,
+  textDark2,
+  textDark3,
+  whiteColor,
+} from '@Shared/constants/colors';
+
+interface ButtonProps {
+  title: string;
+  type: string;
+  size: string;
+  disable?: boolean;
+}
+
+const Button = ({
+  title = 'titleButton',
+  type = 'primary',
+  size = 'long',
+  disable = false,
+  ...nativeProps
+}: ButtonProps & TouchableOpacityProps) => {
+  type = type.toLowerCase();
+  size = size.toLowerCase();
+  return (
+    <TouchableOpacity
+      style={[
+        disable
+          ? styleButtonDisable
+          : [
+              styleButtonActive,
+              [
+                type === 'primary'
+                  ? styles.containerPrimary
+                  : styles.containerSecondary,
+              ],
+            ],
+
+        {
+          width: size === 'long' ? '100%' : '50%',
+        },
+      ]}
+      disabled={disable}
+      {...nativeProps}>
+      <Text
+        style={[
+          disable
+            ? styles.textDisable
+            : type === 'primary'
+            ? styles.textPrimary
+            : styles.textSecondary,
+        ]}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderRadius: 4,
+    alignItems: 'center',
+  },
+  containerDisable: {
+    backgroundColor: textDark3,
+  },
+  containerPrimary: {
+    backgroundColor: bluePrimary,
+  },
+  containerSecondary: {
+    borderWidth: 1,
+    borderColor: bluePrimary,
+    backgroundColor: whiteColor,
+  },
+
+  textPrimary: {
+    color: whiteColor,
+    fontSize: 14,
+    fontWeight: '600',
+  },
+  textSecondary: {
+    color: bluePrimary,
+    fontSize: 14,
+    fontWeight: '600',
+  },
+  textDisable: {
+    color: textDark2,
+    fontSize: 14,
+    fontWeight: '600',
+  },
+});
+
+const styleButtonActive = StyleSheet.flatten([styles.container]);
+const styleButtonDisable = StyleSheet.flatten([
+  styles.containerDisable,
+  styles.container,
+]);
