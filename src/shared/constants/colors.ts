442f51ad802fadfdf11a433b56d61867c1410406
toniebe <toniebe48@gmail.com>
2023-03-11 22:39:02 +0700

    fix[style]:adding some color in shared colors

---
+++ src/shared/constants/colors.ts
@@ -0,0 +1,24 @@
+const bluePrimary = "#039ffb";
+const blueSecondary = "#6bc3fc";
+const blueAlternative3 = "#e7f5fe";
+const textDark = '#697782';
+const textDark1 = "#111733";
+const textDark2 = "#738188";
+const textDark3 = "#9ea2a5";
+const whiteColor = '#ffffff';
+const blackColor = '#111733';
+
+
+
+
+export {
+    bluePrimary,
+    blueSecondary,
+    blueAlternative3,
+    textDark,
+    textDark1,
+    textDark2,
+    textDark3,
+    whiteColor,
+    blackColor
+}
