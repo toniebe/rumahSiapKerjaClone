981cdc28b38bd2a82717d43e6479564b11e8a51c
toniebe <toniebe48@gmail.com>
2023-03-12 03:36:18 +0700

    feat[api]: create api get announcement

---
+++ src/module/home/api/getAnnouncement.ts
@@ -0,0 +1,7 @@
+import { callApi } from "@Shared/helper/callApi";
+import { payloadAnnouncement } from "../types/announcement";
+
+
+export async function getAnnouncement() {
+    return await callApi<payloadAnnouncement>('v3/website/announcements/active', 'GET')
+}
