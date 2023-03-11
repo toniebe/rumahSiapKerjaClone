9a9b27859dbea82dc5aa28b2527f27be7b990aaf
toniebe <toniebe48@gmail.com>
2023-03-11 22:38:07 +0700

    feat[path]: create absolute path

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rule: {
+    'react-native/no-inline-styles': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        'no-inline-styles': false,
+      },
+    ],
+  },
 };
