b2b8a6be52de615dcc74b4a20fa1bc68cc39b171
toniebe <toniebe48@gmail.com>
2023-03-12 03:37:00 +0700

    feat[types]: create announcments types

---
+++ src/module/home/types/announcement.ts
@@ -0,0 +1,31 @@
+export interface announcementProps {
+    id: string;
+    name: string;
+    type: string;
+    description: string;
+    isActive: boolean;
+    startTime: string;
+    endTime: string;
+    callToAction: string;
+    url: string;
+}
+
+export const announcementDefault = {
+    id: "",
+    name: "",
+    type: "",
+    description: "",
+    isActive: false,
+    startTime: "",
+    endTime: "",
+    callToAction: "",
+    url: "",
+}
+
+export interface payloadAnnouncement {
+    success: boolean;
+    message?: string;
+    data: announcementProps;
+}
+
+
