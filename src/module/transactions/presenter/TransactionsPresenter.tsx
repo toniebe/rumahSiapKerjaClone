eb05e84ccd80150894d1491606679b19c1b728e9
toniebe <toniebe48@gmail.com>
2023-03-11 22:43:15 +0700

    feat[presenter]: create transaction presenter

---
+++ src/module/transactions/presenter/TransactionsPresenter.tsx
@@ -0,0 +1,17 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import LayoutScreen from '@Shared/components/organisms/LayoutScreen';
+
+const TransactionsPresenter = () => {
+  return (
+    <LayoutScreen>
+      <View>
+        <Text>TransactionsPresenter</Text>
+      </View>
+    </LayoutScreen>
+  );
+};
+
+export default TransactionsPresenter;
+
+const styles = StyleSheet.create({});
