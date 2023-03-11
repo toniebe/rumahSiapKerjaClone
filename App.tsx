9ced2e29fd0a05ac124c386be5c4b9250762fc7c
toniebe <toniebe48@gmail.com>
2023-03-12 03:38:00 +0700

    fix[library]: try some library for apps

--- App.tsx
+++ App.tsx
@@ -1,24 +1,15 @@
 import {Image, StyleSheet, Text, View} from 'react-native';
-import React, {useEffect} from 'react';
+import React, {Fragment, useEffect} from 'react';
 import Toast from 'react-native-toast-message';
 import {toastConfig} from './src/shared/components/atom/Toast';
-import {BASE_URL} from '@env';
-import IconRumah from './src/shared/assets/icon/IconRumah.svg';
-import {SvgAst, SvgUri} from 'react-native-svg';
 import Core from '@CoreApp/index';
 
 const App = () => {
-  useEffect(() => {
-    Toast.show({
-      type: 'error',
-      text2: 'example an error',
-    });
-  }, []);
   return (
-    <>
-      <Toast config={toastConfig} />
+    <Fragment>
       <Core />
-    </>
+      <Toast config={toastConfig} />
+    </Fragment>
   );
 };
 
