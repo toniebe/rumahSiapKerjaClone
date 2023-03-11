ebab15075c8b2ca4093ed39fe63d8618cc0f6e61
toniebe <toniebe48@gmail.com>
2023-03-12 03:40:30 +0700

    fix[helper]: adding base url from env

--- src/shared/helper/callApi.ts
+++ src/shared/helper/callApi.ts
@@ -1,21 +1,25 @@
 import { Toast } from "react-native-toast-message/lib/src/Toast";
+import { BASE_URL } from "@env";
+
 
 export async function callApi<T>(url: string, method: string, body?: any): Promise<T> {
-    const response = await fetch(url, {
-      method,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(body)
-    });
-  
-    if (!response.ok) {
-      Toast.show({
-        type:'error',
-        text2: 'Ada kendala pada sistem'
-      })
-    }
-  
-    const data = await response.json() as T;
-    return data;
+  const response = await fetch(`${BASE_URL}/${url}`, {
+    method,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  });
+
+  if (!response.ok) {
+    Toast.show({
+      type: 'error',
+      text2: 'Ada kendala pada sistem'
+    })
   }
+
+  const data = await response.json() as T;
+  console.log({data});
+  
+  return data;
+}
